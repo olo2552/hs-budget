8264ebe5e9ba4fdc56cd9b784d528c6f4792f40f
Olgierd "Allgreed" Kasprowicz <olixem@gmail.com>
2019-02-19 20:06:42 +0100

    Add admin interface for easier development

--- Aggregator/main/admin.py
+++ Aggregator/main/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from main.models import Income, Expense
 
-admin.site.register(Income)
-admin.site.register(Expense)
+
+class BudgetItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'amount', 'when')
+
+
+class ExpenseAdmin(BudgetItemAdmin):
+    pass
+
+
+class IncomeAdmin(BudgetItemAdmin):
+    pass
+
+
+admin.site.register(Income, IncomeAdmin)
+admin.site.register(Expense, ExpenseAdmin)
